f6ded1109ff66bcee094cffaa1fa2f60a547232b
Naxthephantom <80404140+Naxthephantom@users.noreply.github.com>
2021-07-09 09:15:52 +0100

    Create word_sorting.py

---
+++ word_sorting.py
@@ -0,0 +1,11 @@
+#program to sort words without the sort function in python
+words = ['phone', 'cone', 'tone',]
+#expected output ['cone', 'phone', 'tone',]
+
+for i in range(0,len(words)):
+    for j in range(0,len(words)):
+        if words[j] > words[i]:
+            temp = words[i]
+            words[i] = words[j]
+            words[j] = temp
+    
